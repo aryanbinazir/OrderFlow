36b42863f5b259c863271f0d672dc26f6d480664
Aryan Binazir <binazir.kobe@gmail.com>
2026-08-10 10:30:41 +0330

    init: create project architecture

---
+++ OrderFlow.API/DI/Swagger.cs
@@ -0,0 +1,20 @@
+using Microsoft.OpenApi;
+
+namespace TicketingService.API.DI
+{
+    public static class Swagger
+    {
+        public static void RegisterSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "OrderFlow API",
+                    Version = "v1",
+                    Description = "OrderFlow REST API"
+                });
+            });
+        }
+    }
+}
